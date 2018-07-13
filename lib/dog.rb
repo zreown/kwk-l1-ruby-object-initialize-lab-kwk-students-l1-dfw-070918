ed92e28c9f2952fde3313d662df78e9b7cc4d680
zreown <zoe.reown@gmail.com>
2018-07-13 19:09:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,10 @@
 # dog.rb
+
+class Dog 
+  def initialize(name, breed = "Mutt")
+    @name = name 
+    @breed = breed 
+  end
+end 
+    
+    
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,9 @@
 # person.rb
+
+class Person 
+  def initialize(name)
+    @name = name
+  end 
+end
+    
+
